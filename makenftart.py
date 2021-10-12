e90b9cfe08fa325cb86184fa2f041bac09bfe1b4
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2021-10-12 16:12:36 +0300

    Update makenftart.py

--- makenftart.py
+++ makenftart.py
@@ -1,6 +1,5 @@
-Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import matplotlib.pyplot as plt
+
+import matplotlib.pyplot as plt
 from PIL import Image
 
 array = ['C:\choc.png','C:\mario.jpg','C:\goal.jpg']
